d3a7ea04d76e42f5dabe1981b8652b47c56dfdea
Jonthesallz <gitboi56@outlook.com>
2022-04-15 20:50:19 +0100

    changed port

--- index.js
+++ index.js
@@ -68,6 +68,8 @@ app.delete('/todos/:id', async (req, res) => {
 })
 
 
+const port = process.env.PORT || 3000;
+
 app.listen(3000, () => {
-    console.log("APP IS LISTENING ON PORT 3000!")
+    console.log(`app is listening on port: ${port}`)
 })
